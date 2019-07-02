41e3042a4854df5e4dc2982adfbf0a7a72e2f895
EmilioKolo <emilio_193@hotmail.com>
2019-07-02 20:07:04 -0300

    Add files via upload
    Versiones corregidas de la red de Sanger del final de clase del 2 de julio

---
+++ aprendizaje-no-supervisado/OjaSanger.py
@@ -0,0 +1,169 @@
+import math
+import numpy as np
+import random
+import os
+
+
+class redOjaSanger(object):
+        '''
+        Red Neuronal para calculo de componentes principales
+        '''
+        #Inicializa la matriz de pesos con valores pequeños
+        def __init__(self,Dim=[2,2,1],w_init=10,beta=1,verbose=7):
+                #Dim es una lista con la dimension de cada capa
+                #Dim[-1]==output // Dim[0]==input
+                #w_init es un maximo/minimo para los pesos iniciales 
+                #(Se generan entre w_init/100 y -w_init/100)
+                self.w_ = [];
+                for i in range(len(Dim)-1):
+                        #Llena self.w_ con un array por espacio entre capas (len(Dim)-1 capas)
+                        #Cada elemento de self.w_ es un array de numpy con todos los pesos de esa capa (iniciados al azar)
+                        self.w_.append(np.array([[random.randint(-w_init,w_init)/100 for i in range(Dim[i]+1)] for j in range(Dim[i+1])]));
+
+                self.Dim = Dim;
+                self.beta = beta;
+                #Esto es alto para testing
+                self.verbose = verbose;
+
+                print('\nLa red neuronal fue creada con exito.')
+                if self.verbose >= 10:
+                        print('Matriz de pesos (incluye bias):\n')
+                        for x in self.w_:
+                                print(x)
+                print('')
+
+        #Derivada de la funcion identidad
+        def _derivada_identidad(self,n,b):
+                return b
+
+        #Funcion identidad
+        def _funcion_identidad(self,n,b):
+                return n*b
+
+        #Funcion que hace la sumatoria de w(ki)*y(k) para todos los valores de output
+        def _suma_Oja(self,output,y):
+                cont = 0;
+                #Se recorre cada posicion del output
+                #z es k en la teorica
+                for z in range(len(output)):
+                        cont += self.w_OjaSanger[z,y]*output[z];
+                return cont
+
+        #Funcion para actualizar los pesos segun la regla de Oja
+        def _Oja_update(self,n_train,current,output):
+                #Se recorre cada fila de la matriz de pesos (j en teorica)
+                for x in range(len(self.w_OjaSanger)):
+                        #Se recorre cada entrada de cada fila de la matriz de pesos (cada columna, i en teorica)
+                        for y in range(len(self.w_OjaSanger[x])):
+                                self.w_OjaSanger[x,y] += n_train*output[x]*(current[y]-self._suma_Oja(output,y))
+                return self
+
+        #Funcion para actualizar los pesos segun la regla de Sanger
+        def _Sanger_update(self,n_train,current,output):
+                #Se recorre cada fila de la matriz de pesos (j en teorica)
+                for x in range(len(self.w_OjaSanger)):
+                        #Se recorre cada entrada de cada fila de la matriz de pesos (cada columna, i en teorica)
+                        for y in range(len(self.w_OjaSanger[x])):
+                                self.w_OjaSanger[x,y] += n_train*output[x]*(current[y]-self._suma_Sanger(output,y,x)-self.w_OjaSanger[x,y]*output[x])
+                return self
+
+        #Funcion que hace la sumatoria de w(ki)*y(k) para los valores de output menores a j-1 (j en teorica)
+        def _suma_Sanger(self,output,y,x):
+                cont = 0;
+                #Se recorre cada valor del output hasta la neurona x-1 (j-1 en teorica)
+                #z es k en la teorica
+                for z in range(x):
+                        cont += self.w_OjaSanger[z,y]*output[z];
+                return cont
+
+        #Funcion para revisar si el cambio de pesos entre epocas es significativo
+        def _w_OjaSanger_check(self,tolerance):
+                change = 0;
+                #Se recorre cada fila de la matriz de pesos
+                for x in range(len(self.w_OjaSanger_log_[-1])):
+                        #Se recorre cada entrada de cada fila de la matriz de pesos (cada columna)
+                        for y in range(len(self.w_OjaSanger_log_[-1][x])):
+                                change += (self.w_OjaSanger_log_[-1][x][y]-self.w_OjaSanger_log_[-2][x][y])**2
+                if math.sqrt(change) <= tolerance:
+                        print('El valor de cambio fue menor al tolerado. Se termina el entrenamiento.\nCambio del vector:')
+                        print(math.sqrt(change))
+                return math.sqrt(change) > tolerance
+
+        #Funcion que devuelve n componentes principales segun regla de Oja
+        def OjaSanger(self,n,L_in,n_train=0.05,w_tolerance=0.001,w_init=50,OjaSanger=0):
+                #n es la cantidad de componentes principales que devuelve la funcion
+                #L_in es la lista que contiene los datos (solo input)
+                #L_in debe estar centrado en 0
+
+                L_in_interna = list(np.copy(L_in));
+
+                #Primero inicializo los pesos de los componentes principales
+                self.w_OjaSanger = np.array([[random.randint(-w_init,w_init)/100 for x in range(len(L_in_interna[0]))] for y in range(n)]);
+
+                print('\nLa red neuronal para Oja-Sanger fue creada con exito.')
+                if self.verbose >= 7:
+                        print('Matriz de pesos (no incluye bias):')
+                        for x in self.w_OjaSanger:
+                                print(x)
+                print('')
+
+                #Inicializo el log de pesos
+                self.w_OjaSanger_log_ = [np.copy(self.w_OjaSanger)];
+
+                #i cuenta un minimo de iteraciones por si se aplana la funcion al principio
+                i=0;
+                #b revisa que el cambio de pesos sea significativo
+                b=True;
+                while b:
+
+                        #Coeficiente de correccion del n_train
+                        corr_n = n_train*w_tolerance*i;
+
+                        #Una vez cada vuelta por L_in se reordena la lista
+                        if i%len(L_in_interna) == 0:
+                                random.shuffle(L_in_interna);
+                                if self.verbose >= 7 and i > 0:
+                                        print('Iteracion numero ' + str(i));
+
+                        #current representa el input actual
+                        current = L_in_interna[i%len(L_in_interna)];
+
+                        #output es el resultado de input*pesos
+                        output = np.dot(self.w_OjaSanger,current);
+
+                        #Despues actualizo el peso
+                        if OjaSanger == 0:
+                                #Actualizacion segun regla de Oja
+                                self._Oja_update(n_train,current,output);
+                        elif OjaSanger == 1:
+                                #Actualizacion segun regla de Sanger
+                                self._Sanger_update(n_train-corr_n,current,output);
+                        else:
+                                print('Mensaje de error 1/2 por variable OjaSanger.')
+
+                        if self.verbose >= 8:
+                                print('Matriz de pesos actualizada:\n')
+                                for k in self.w_OjaSanger:
+                                        print(k)
+                                print('')
+
+                        #Se agrega el peso actualizado al log
+                        self.w_OjaSanger_log_.append(np.copy(self.w_OjaSanger));
+
+                        #Reviso si el cambio de pesos es significativo
+                        b = i < int(math.sqrt(len(L_in_interna))) or self._w_OjaSanger_check(w_tolerance);
+
+                        i+=1;
+
+                if OjaSanger == 0:
+                        print('\nSe ha finalizado el entrenamiento segun la regla de Oja.\nPesos finales:');
+                elif OjaSanger == 1:
+                        print('\nSe ha finalizado el entrenamiento segun la regla de Sanger.\nPesos finales:');
+                else:
+                        print('Mensaje de error 2/2 por variable OjaSanger.')
+
+                for x in self.w_OjaSanger:
+                        print(x);
+
+                return self.w_OjaSanger
+
--- aprendizaje-no-supervisado/SangerTP2Emilio.py
+++ aprendizaje-no-supervisado/SangerTP2Emilio.py
@@ -1,182 +1,86 @@
-from ClaseOjaSangerEmilio import *
+
+from OjaSanger import *
+from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 import os
-import numpy as np
-from mpl_toolkits.mplot3d import axes3d
+
 ###################################################################################
-class RunnerTP2(object):
-    def __init__(self, l_rate=0.01, w_tolerance=0.0001, w_init=100, OjaSanger=1, principal_components=3):
-        # Numero de componentes principales a extraer
-        self.pcs = principal_components;
 
-        # Variables de entrenamiento
-        self.l_rate = l_rate
-        self.w_tolerance = w_tolerance
-        self.w_init = w_init
-        self.OjaSanger = OjaSanger  # 0 para Oja, 1 para Sanger
-        self.weights = []
-        self.salida = []
+#Nombre del archivo con los sets de entrenamiento
+nombre = 'tp2_training_dataset.csv';
 
-    # Entrena y guarda los datos
-    def train(self, filename='data/tp2_training_dataset.csv'):
-        # Abro el archivo
-        with open(os.path.dirname(os.path.abspath(__file__)) + '/' + str(filename), 'r') as F:
-            L = F.readlines()
-            L_out = []
-            L_in = []
-            for i in range(len(L)):
-                L[i] = L[i].rstrip('\n').split(',')
-                L_out.append(L[i][0])
-                L_in.append(L[i][1:])
+#Numero de componentes principales a extraer
+n = 9;
 
-        # Transformo los datos de input en numeros
-        for i in range(len(L_in)):
-            for j in range(len(L_in[i])):
-                L_in[i][j] = int(L_in[i][j])
-        self.L_out = L_out
-        # Inicializo la red y la entreno
-        ros = redOjaSanger(verbose=7)
-        W = ros.OjaSanger(self.pcs, L_in, n_train=self.l_rate, w_tolerance=self.w_tolerance,
-                          w_init=self.w_init, OjaSanger=self.OjaSanger)
-        self.weights = W
+#Variables de entrenamiento
+n_train = 0.02;
+w_tolerance = 0.0001;
+w_init = 50;
+OjaSanger = 1; # 0 para Oja, 1 para Sanger
 
-        # Hago una lista con los resultados de la red para todos los datos de L_in
-        salida = []
-        for i in L_in:
-            salida.append(np.dot(W, i))
-        self.salida = salida
-        self.save_results()
+#Abro el archivo
+with open(os.path.dirname(os.path.abspath(__file__)) + '/' + str(nombre),'r') as F:
+    L = F.readlines();
+    L_out = [];
+    L_in = [];
+    for i in range(len(L)):
+        L[i] = L[i].rstrip('\n').split(',');
+        L_out.append(L[i][0]);
+        L_in.append(L[i][1:]);
 
-    def plot(self):
-        ###Lista cmaps### (https://matplotlib.org/gallery/color/colormap_reference.html)
-        # Diverging:
-        # 'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu',
-        # 'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic'
+#Transformo los datos de input en numeros
+for i in range(len(L_in)):
+    L_out[i] = int(L_out[i])
+    for j in range(len(L_in[i])):
+        L_in[i][j] = int(L_in[i][j]);
 
-        # Perceptually Uniform Sequential:
-        # 'viridis', 'plasma', 'inferno', 'magma', 'cividis'
+#Inicializo la red y la entreno
+ros = redOjaSanger(verbose=7);
+W = ros.OjaSanger(n,L_in,n_train=n_train,w_tolerance=w_tolerance,
+                  w_init=w_init,OjaSanger=OjaSanger);
 
-        # Qualitative:
-        # 'Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2', 'Set1',
-        # 'Set2', 'Set3', 'tab10', 'tab20', 'tab20b', 'tab20c'
-        cmap = 'tab10'  # Todavia no se cual queda mejor
+#Lista de colores para el grafico
+clist = [int(i)/9 for i in L_out];
 
-        # Lista de colores para el grafico
-        clist = [int(i) / 9 for i in self.L_out]
+###Lista cmaps### (https://matplotlib.org/gallery/color/colormap_reference.html)
+#Diverging:
+#'PiYG', 'PRGn', 'BrBG', 'PuOr', 'RdGy', 'RdBu', 'RdYlBu',
+#'RdYlGn', 'Spectral', 'coolwarm', 'bwr', 'seismic'
 
-        # Paso los resultados a tres listas de dimensiones
-        xdata = []
-        ydata = []
-        zdata = []
-        for i in self.salida:
-            xdata.append(i[0])
-            ydata.append(i[1])
-            zdata.append(i[2])
+#Perceptually Uniform Sequential:
+#'viridis', 'plasma', 'inferno', 'magma', 'cividis'
 
-        ax = plt.axes(projection='3d')
-        ax.scatter3D(xdata, ydata, zdata, c=clist, cmap=cmap)
-        plt.show()
+#Qualitative:
+#'Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2', 'Set1',
+#'Set2', 'Set3', 'tab10', 'tab20', 'tab20b', 'tab20c'
 
-    # TODO si el nombre del archivo coincide se pisan los datos, se podria preguntar si se quiere pisar o guardar con otro valor
-    def save_results(self):
-        filename = "redes_tp2/" + \
-                   "w_tolerance" + str(self.w_tolerance) + \
-                   "l_rate" + str(self.l_rate) + \
-                   "OjaSanger" + str(self.OjaSanger) + \
-                   "w_init" + str(self.w_init) + \
-                   "pcs" + str(self.pcs) + \
-                   ".npz"
-        np.savez(filename, w_tolerance=self.w_tolerance, l_rate=self.l_rate,
-                 OjaSanger=self.OjaSanger, w_init=self.w_init,
-                 salida=self.salida, weights=self.weights, pcs=self.pcs, L_out=self.L_out)
+cmap = 'tab10'; #Todavia no se cual queda mejor
 
-    def load_network(self, filename):
-        data = np.load(filename)
-        self.w_tolerance = data['w_tolerance'] or 0.0001
-        self.l_rate = data['l_rate'] or 0.01
-        self.OjaSanger = data['OjaSanger'] or 1
-        self.w_init = data['w_init'] or 100
-        self.pcs = data['pcs'] or 3
-        self.weights = data['weights']
-        self.salida = data['salida']
-        self.L_out = data['L_out']
-        #TODO agregar L_in??
+#Hago una lista con los resultados de la red para todos los datos de L_in
+salida = []
+for i in L_in:
+    salida.append(np.dot(W,i));
+'''
+#Paso los resultados a tres listas de dimensiones
+xdata = [];
+ydata = [];
+zdata = [];
+for i in salida:
+    xdata.append(i[0]);
+    ydata.append(i[1]);
+    zdata.append(i[2]);
 
-    def datos_red(self):
-        print("W_tolerance: {0}, learning_rate: {1} \n"
-              "PCS: {2} \n"
-              "W_init: {3} \n".format(self.w_tolerance, self.l_rate, self.pcs, self.w_init))
-        if self.OjaSanger == 1:
-            print("Regla de Sanger")
-        else:
-            print("Regla de Oja")
+ax = plt.axes(projection='3d')
+ax.scatter3D(xdata, ydata, zdata, c=clist, cmap=cmap);
+plt.show()
+'''
+R = np.zeros(shape=(900,n+1))
+for i in range(len(salida)):
+    R[i,n] = L_out[i];
+    for j in range(len(salida[i])):
+        R[i,j] = salida[i][j];
 
-# Auxiliares
-import glob
+print(R[:10])
 
-def opcion_entrenar():
-    learning_rate = 0
-    while learning_rate <= 0:
-        learning_rate = float(input("Ingrese learning rate: "))
-    print("Learning rate: {0}".format(learning_rate))
-    w_tolerance = 0
-    while w_tolerance <= 0:
-        w_tolerance = float(input("Ingrese tolerancia de error: "))
-    print("Tolerance: {0}".format(w_tolerance))
-    w_init = 0
-    while w_init <= 0:
-        w_init = int(input("Ingrese w_init: "))
-    print("W_init: {0}".format(w_init))
-    OjaSanger = -1
-    while OjaSanger != 0 and OjaSanger != 1:
-        OjaSanger = int(input("Elija metodo para PCA (1-Sanger, 0-Oja: "))
-    print("OjaSanger: {0}".format(OjaSanger))
-    pcs = 0
-    while pcs <= 0:
-        pcs = int(input("Ingrese Nro de componentes principales a extraer: "))
-    print("pcs: {0}".format(pcs))
+np.save('CP' + str(n),R)
 
-    net = RunnerTP2(learning_rate, w_tolerance, w_init, OjaSanger, pcs)
-    net.train()
-    return net
-
-def opcion_cargar():
-    archivos = (glob.glob("redes_tp2/*.npz"))
-    if len(archivos) == 0:
-        print("No hay archivos para cargar.")
-    else:
-        for index, file in enumerate(archivos):
-            print("{0} - {1}".format(index, file))
-        nro_archivo = -1
-        while nro_archivo <= -1 or nro_archivo > len(archivos):
-            nro_archivo = int(input("Elija nro de archivo 0 a {0}: ".format(len(archivos) - 1)))
-
-        net = RunnerTP2()
-        net.load_network(archivos[nro_archivo])
-        print("Cargado: {0}".format(archivos[nro_archivo]))
-        net.plot()
-    return net
-
-# MAIN
-
-salir = 0
-while not salir:
-    opcion = 0
-    while opcion <= 0 or opcion > 5:
-        opcion = int(input("\n 1-Entrenar\n 2-Cargar red desde archivo + Plot\n 3-Plot + "
-                           "datos red actual \n 4-Entrenar con opciones por default \n 5-Salir \n Seleccione opcion: "))
-    if opcion == 1:
-        net = opcion_entrenar()
-    elif opcion == 2:
-        net = opcion_cargar()
-    elif opcion == 3:
-        if ('net' in locals()):
-            net.datos_red()
-            net.plot()
-        else:
-            print("No hay red cargada")
-    elif opcion == 4:
-        net = RunnerTP2()
-        net.train()
-    else:
-        salir = 1
